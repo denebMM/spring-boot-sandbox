388a0a79de7c7a25d3cbffea42ca30acb47d7449
Deneb Moliner Morales <152557856+denebMM@users.noreply.github.com>
2025-08-14 22:20:47 -0600

    Initial commit

---
+++ src/main/java/sandbox/dao/UsuarioDao.java
@@ -0,0 +1,17 @@
+package sandbox.dao;
+
+import sandbox.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+
+
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
+}
